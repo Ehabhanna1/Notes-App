2574716132cf249357479ac64c6a5671f8e8f911
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-23 18:12:52 +0200

    create add notes state

--- lib/logic/add_notes/cubit/add_notes_state.dart
+++ lib/logic/add_notes/cubit/add_notes_state.dart
@@ -4,3 +4,13 @@ part of 'add_notes_cubit.dart';
 sealed class AddNotesState {}
 
 final class AddNotesInitial extends AddNotesState {}
+
+final class AddNotesLoading extends AddNotesState {}
+
+final class AddNotesSuccess extends AddNotesState {}
+
+final class AddNotesError extends AddNotesState {
+  final String errorMessage;
+
+  AddNotesError(this.errorMessage);
+}
