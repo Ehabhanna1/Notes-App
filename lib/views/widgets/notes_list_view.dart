0fafe90aac0b51013e7918a8f67091231d4fe24e
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-23 13:34:35 +0200

    add notes list view

---
+++ lib/views/widgets/notes_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 7,
+        itemBuilder: (context,index){
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 15),
+            child: const NoteItem(),
+          );
+        }
+        ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
-import 'package:notes_app/views/widgets/note_item.dart';
+import 'package:notes_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -17,13 +17,14 @@ class NotesViewBody extends StatelessWidget {
          const CustomAppBar(),
          const SizedBox(height: 20,),
 
-         const NoteItem(),
+         const NotesListView(),
       
         ],
       ),
     );
   }
 }
+
 
 
 
